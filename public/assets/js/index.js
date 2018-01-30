0402152fb32a8779c107ae47a62183b04afb9e0e
Lisa Zapson <lisazapson@ymail.com>
2018-01-30 08:25:12 -0500

    filter-menu changes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -12,8 +12,34 @@ $(document).ready(function() {
     }   
   });
 
+  $("#filter-menu-submit").on("click", function() {
+    var filterObj = {
+      county: $("#county").val().trim(),
+      address: $("#address").val().trim(),
+      city: $("#city").val().trim(),
+      state: $("#state").val().trim(),
+      zip: $("#zip").val().trim(),
+      party: $("#party").val().trim(),
+      status: $("#status").val().trim(),
+      age: $("#age").val().trim(),
+      ward: $("#ward").val().trim(),
+      district: $("#district").val().trim(),
+      cd: $("#cd").val().trim(),
+      ld: $("#ld").val().trim(),
+      freeholder: $("#freeholder").val().trim(),
+      schoolDistrict: $("#school-district").val().trim(),
+      regSchoolDistrict: $("#reg-school-district").val().trim(),
+      fire: $("#fire-district").val().trim()
+      }
 
-  
+      console.log(filterObj);
+      $.post("/api/filter", filterObj, function() {
+
+      });
+
+
+  });
+
 
   // Filter Menu
 
