57d0d0d9f7cb7c7c578ceb111c0b7d08f1f40b78
Lindsay Brown <lindsayluwho@gmail.com>
2018-02-02 10:36:34 -0500

    fix

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,15 @@ var mysql = require("mysql");
 
 if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);}
-    else {
+else {
     var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "voteknockerDb"
-});
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "voteknockerDb"
+    });
+}
 
 // Make connection.
 connection.connect(function (err) {
