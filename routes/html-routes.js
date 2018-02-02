d02e3422bce2bac24a3df6ccd336ba5bd87b31f5
Lisa Zapson <lisazapson@ymail.com>
2018-02-02 12:01:12 -0500

    status update

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -24,7 +24,7 @@ module.exports = function(app) {
 
   app.get("/status/:id", function(req, res) {
       var voterId = req.params.id;
-      connection.query("SELECT * FROM voterhistories2 WHERE voterId =?", voterId, function(err, result) {
+      connection.query("SELECT * FROM alphavoters2 WHERE voterId =?", voterId, function(err, result) {
         console.log(result[0]);
         res.render("status", result[0]);
       });
@@ -37,7 +37,7 @@ module.exports = function(app) {
     
   app.get("/interactions/:id", function(req, res) {
       var voterId = req.params.id;
-      connection.query("SELECT * FROM voterhistories2 WHERE voterId =?", voterId, function(err, result) {
+      connection.query("SELECT * FROM alphavoters2 WHERE voterId =?", voterId, function(err, result) {
         res.render("interactions", result[0]);
       });
     });
